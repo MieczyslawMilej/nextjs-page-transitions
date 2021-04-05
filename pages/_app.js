26e1f68e33d282c60c2f597c1860f53e45298159
MieczyslawMilej <mieczyslaw.milej@gmail.com>
2021-04-05 20:15:44 +0200

    Created animation for products page

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import React from "react";
 import App from "next/app";
+import { AnimatePresence } from "framer-motion";
 import "../styles.scss";
 
 class MyApp extends App {
-  render() {
-    const { Component, pageProps, router } = this.props;
+	render() {
+		const { Component, pageProps, router } = this.props;
 
-    return <Component {...pageProps} key={router.route} />;
-  }
+		return (
+			<AnimatePresence exitBeforeEnter>
+				<Component {...pageProps} key={router.route} />;
+			</AnimatePresence>
+		);
+	}
 }
 
 export default MyApp;
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,55 +1,73 @@
+import { motion } from "framer-motion";
 import fetch from "isomorphic-unfetch";
 import Link from "next/link";
 
 const Product = props => (
-  <div>
-    <div className='fullscreen'>
-      <div className='product'>
-        <div className='img'>
-          <img key={props.product.image} src={props.product.image} />
-        </div>
-        <div className='product-details'>
-          <div className='inner'>
-            <Link href='/'>
-              <div>
-                <a className='go-back'>Back to products</a>
-              </div>
-            </Link>
-            <div>
-              <span className='category'>Protein</span>
-            </div>
-            <h1>{props.product.name}</h1>
-            <p>{props.product.details}</p>
-            <div className='additonals'>
-              <span>Soy Free</span>
-              <span>Gluten Free</span>
-            </div>
-            <div className='qty-price'>
-              <div className='qty'>
-                <div className='minus'>-</div>
-                <div className='amount'>1</div>
-                <div className='add'>+</div>
-              </div>
-              <span className='price'>{props.product.price}</span>
-            </div>
-            <div className='btn-row'>
-              <button className='add-to-cart'> Add to cart</button>
-              <button className='subscribe'> Subscribe</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<motion.div exit={{ opacity: 0 }}>
+		<div className="fullscreen">
+			<div className="product">
+				<div className="img">
+					<motion.img
+						initial={{ x: 200, opacity: 0 }}
+						animate={{ x: 0, opacity: 1 }}
+						key={props.product.image}
+						src={props.product.image}
+					/>
+				</div>
+				<div className="product-details">
+					<div className="inner">
+						<Link href="/">
+							<div>
+								<a className="go-back">
+									Back to products
+								</a>
+							</div>
+						</Link>
+						<div>
+							<span className="category">
+								Protein
+							</span>
+						</div>
+						<h1>{props.product.name}</h1>
+						<p>{props.product.details}</p>
+						<div className="additonals">
+							<span>Soy Free</span>
+							<span>Gluten Free</span>
+						</div>
+						<div className="qty-price">
+							<div className="qty">
+								<div className="minus">-</div>
+								<div className="amount">1</div>
+								<div className="add">+</div>
+							</div>
+							<span className="price">
+								{props.product.price}
+							</span>
+						</div>
+						<div className="btn-row">
+							<button className="add-to-cart">
+								{" "}
+								Add to cart
+							</button>
+							<button className="subscribe">
+								{" "}
+								Subscribe
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</motion.div>
 );
 
-Product.getInitialProps = async function(context) {
-  const { id } = context.query;
-  const res = await fetch(
-    `http://my-json-server.typicode.com/wrongakram/demo/products/${id}`
-  );
-  const product = await res.json();
-  return { product };
+Product.getInitialProps = async function (context) {
+	const { id } = context.query;
+	const res = await fetch(
+		`http://my-json-server.typicode.com/MieczyslawMilej/nextjs-page-transitions/db/${id}`
+	);
+	const product = await res.json();
+	return { product };
 };
 
 export default Product;
